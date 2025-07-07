10a66dd3541d5edfc2096fb1eca6a36e8cd7e010
AbidShazid3 <abidshazid3@gmail.com>
2025-07-07 07:04:31 +0600

    added baseUrl

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-server-rust.vercel.app/api' }),
     tagTypes: ['book', 'borrow'],
     endpoints: (builder) => ({
         getAllBooks: builder.query({
